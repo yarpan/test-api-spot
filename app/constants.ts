bdcdf3d44a951febe29cce0c9f08031eb882bf4e
Yaroslav Panchishyn <yarpan@gmail.com>
2025-10-07 23:16:28 +0300

    constants

---
+++ app/constants.ts
@@ -0,0 +1,15 @@
+
+export const BASE_URL = "https://api.spotify.com/v1";
+
+export const ENDPOINTS = {
+  audiobooks: "/audiobooks/",
+  me: "/me",
+  users: "/users",
+  playlists: "/playlists/",
+  shows: "/shows/",
+  search: "/search",
+};
+
+export const TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+export const LOGIN_URL = "https://accounts.spotify.com/en/login";
